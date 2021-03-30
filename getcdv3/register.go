048f4a3e123532823bbe624ec2b30520077c8b4f
skiffer-git <72860476+skiffer-git@users.noreply.github.com>
2021-03-30 15:55:18 +0800

    Add files via upload

---
+++ getcdv3/register.go
@@ -0,0 +1,84 @@
+package getcdv3
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"net"
+	"strconv"
+	"strings"
+)
+
+type RegEtcd struct{
+	cli *clientv3.Client
+	ctx	context.Context
+	cancel context.CancelFunc
+	key string
+}
+var rEtcd *RegEtcd
+
+//    "%s:///%s"
+func GetPrefix(schema, serviceName string)(string){
+	return fmt.Sprintf("%s:///%s/", schema, serviceName)
+}
+
+//etcdAddr separated by commas
+func RegisterEtcd(schema, etcdAddr, myHost string, myPort int, serviceName string, ttl int)(error){
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints: strings.Split(etcdAddr, ","),
+	})
+	if err != nil {
+		//		return fmt.Errorf("grpclb: create clientv3 client failed: %v", err)
+		return fmt.Errorf("create etcd clientv3 client failed, errmsg:%v, etcd addr:%s", err, etcdAddr)
+	}
+
+	//lease
+	ctx, cancel := context.WithCancel(context.Background())
+	resp, err := cli.Grant(ctx, int64(ttl))
+	if(err != nil){
+		return fmt.Errorf("grant failed")
+	}
+
+	//  schema:///serviceName/ip:port ->ip:port
+	serviceValue := net.JoinHostPort(myHost,  strconv.Itoa(myPort))
+	serviceKey := GetPrefix(schema, serviceName)+serviceValue
+
+	//set key->value
+	if _, err := cli.Put(ctx, serviceKey, serviceValue, clientv3.WithLease(resp.ID)); err != nil {
+		return fmt.Errorf("put failed, errmsg:%v， key:%s, value:%s", err, serviceKey, serviceValue)
+	}
+
+	//keepalive
+	kresp, err := cli.KeepAlive(ctx, resp.ID);
+	if  err != nil {
+		return fmt.Errorf("keepalive faild, errmsg:%v, lease id:%d", err, resp.ID)
+	}
+
+	go func() {
+	FLOOP:
+		for {
+			select {
+			case _, ok := <-kresp:
+				if ok == true{
+				//	fmt.Println("keepalive resp: ", r)
+				} else {
+					break FLOOP
+				}
+			default:
+			}
+		}
+	}()
+
+	rEtcd = &RegEtcd{ctx: ctx,
+		cli:cli,
+		cancel:cancel,
+		key:serviceKey}
+
+	return nil
+}
+
+func UnRegisterEtcd(){
+	//delete
+	rEtcd.cancel()
+	rEtcd.cli.Delete(rEtcd.ctx, rEtcd.key)
+}
---
+++ getcdv3/resolver.go
@@ -0,0 +1,134 @@
+package getcdv3
+
+import (
+	"context"
+	"fmt"
+	"go.etcd.io/etcd/clientv3"
+	"go.etcd.io/etcd/mvcc/mvccpb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/balancer/roundrobin"
+	"google.golang.org/grpc/resolver"
+	"strings"
+	"sync"
+	"time"
+)
+
+type Resolver struct {
+	etcdAddr string
+	addrDict map[string]resolver.Address
+	cli     *clientv3.Client
+	cc      resolver.ClientConn
+	serviceName string
+	schema string
+	watchStartRevision int64
+}
+
+var (
+	mu      sync.Mutex
+	allPrefix map[string]int = make (map[string]int)
+)
+
+func (r1 *Resolver) ResolveNow(rn resolver.ResolveNowOptions) {
+}
+
+func (r1 *Resolver) Close() {
+}
+
+func (r *Resolver) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error){
+	//etcd client
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints: strings.Split(r.etcdAddr, ","),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("etcd clientv3 client failed: %v, etcd:%s", err, target)
+	}
+
+	r.cli  = cli
+	r.cc = cc
+	r.addrDict =  make(map[string]resolver.Address)
+
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	//     "%s:///%s"
+	prefix := GetPrefix(r.schema, r.serviceName)
+	// get key first
+	resp, err := r.cli.Get(ctx, prefix, clientv3.WithPrefix())
+
+	if err == nil {
+		for i := range resp.Kvs {
+		//	fmt.Println(resp.Kvs[i].Value)
+			r.addrDict[string(resp.Kvs[i].Value)] = resolver.Address{Addr: string(resp.Kvs[i].Value)}
+		}
+		r.update(r.addrDict)
+		r.watchStartRevision = resp.Header.Revision + 1
+	//	fmt.Println(resp.Header.Revision)
+	}else {
+		return nil, fmt.Errorf("etcd get failed, prefix: %s", prefix)
+	}
+
+
+	//goroutine watch
+	go r.watch(prefix)
+	return r, nil
+}
+
+func (r *Resolver) update(addrDict map[string]resolver.Address){
+	addrList := make([]resolver.Address, 0, len(addrDict))
+	for _, v := range addrDict {
+		addrList = append(addrList, v)
+	}
+	r.cc.UpdateState(resolver.State{Addresses: addrList})
+}
+
+func(r*Resolver) Scheme() string{
+	return r.schema
+}
+
+
+func (r *Resolver) watch(prefix string) {
+	//only one goroutine for same prefix
+	mu.Lock()
+	_, ok := allPrefix[prefix]
+	if ok {
+		mu.Unlock()
+		return
+	} else {
+		allPrefix[prefix] = 1
+	}
+	mu.Unlock()
+
+	rch := r.cli.Watch(context.Background(), prefix, clientv3.WithPrefix(), clientv3.WithRev(r.watchStartRevision))
+	for n := range rch {
+		for _, ev := range n.Events {
+			switch ev.Type {
+			case mvccpb.PUT:
+				(r.addrDict)[string(ev.Kv.Key)] = resolver.Address{Addr: string(ev.Kv.Value)}
+			case mvccpb.DELETE:
+				delete(r.addrDict, string(ev.Kv.Key))
+			}
+		}
+		r.update(r.addrDict)
+	}
+}
+
+func GetBuild(schema, etcdaddr, servicename string) (*Resolver){
+	r := new(Resolver)
+	r.etcdAddr = etcdaddr
+	r.schema = schema
+	r.serviceName = servicename
+	return r
+}
+
+func GetConn(schema, etcdaddr, servicename string) (*grpc.ClientConn){
+	resolver.Register(GetBuild(schema, etcdaddr, servicename))
+	conn, err := grpc.Dial(
+		GetPrefix(schema, servicename),
+		grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"LoadBalancingPolicy": "%s"}`, roundrobin.Name)),
+		grpc.WithInsecure(),
+		grpc.WithTimeout(time.Duration(5)*time.Second),
+	)
+	if(err != nil) {
+		return nil
+	}
+	return conn
+}
+
